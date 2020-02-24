0b41c6b87112667e35c415538bec183ca88f96c0
Jonathan Karr <jonrkarr@gmail.com>
2020-02-24 14:03:14 -0500

    updating for consistency with semantic versioning

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ name = 'wc_cli'
 dirname = os.path.dirname(__file__)
 package_data = {
     name: [
-        'VERSION',
         'config/*.cfg',
     ],
 }
--- wc_cli/__init__.py
+++ wc_cli/__init__.py
@@ -1,7 +1,6 @@
 import pkg_resources
 
 # read version
-with open(pkg_resources.resource_filename('wc_cli', 'VERSION'), 'r') as file:
-    __version__ = file.read().strip()
+from ._version import __version__
 
 from . import config
